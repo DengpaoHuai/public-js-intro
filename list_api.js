d299c74e58667ec7648b685401ba06b97786f951
Pierrick Hauguel <pierrickhauguel@outlook.com>
2024-05-23 18:40:12 +0200

    ajout des consignes

---
+++ list_api.js
@@ -0,0 +1,54 @@
+//les images de shiba
+//url : http://shibe.online/api/shibes?count=[1-100]&urls=[true/false]&httpsUrls=[true/false]
+
+/*
+const getData = async () => {
+  let response = await fetch("https://swapi-api.hbtn.io/api/planets/");
+  console.log(response);
+  const results = await response.json();
+  //ici, ça change !!!!
+};
+
+getData();*/
+// UNIQUEMENT POUR LES SHIBAS
+/*
+Le retour de l'api ne renvoi que l'ID de l'image.
+Voici le pattern à respecter pour afficher l'image.
+Vous devez ajouter devant l'ID ce lien : 
+https://cdn.shibe.online/shibes/
+et après l'ID l'extension soit .jpg
+
+Voici une url complète: 
+https://cdn.shibe.online/shibes/8f5e8265d2e0ea74f19af2454a4a8816fd4039d8.jpg
+
+voici comment concaténer une chaine de caractère en JS : 
+
+"https://cdn.shibe.online/shibes/" + nomVariable + ".jpg"
+
+*/
+
+//HARRY POTTER
+
+/*
+
+https://hp-api.onrender.com/api/characters
+Ici, reprenez l'exemple de la liste des planètes.
+C'est presque pareil :)
+
+*/
+
+//API Météo
+
+/*
+https://goweather.herokuapp.com/weather/Rennes
+
+Après le /weather/ ajoutez le nom de la ville
+oubliez pas le console.log pour voir ce qu'il y a à l'interieur
+
+*/
+
+/* Savoir lire un swagger :
+https://api.seriesquotes.10cyrilc.me/docs
+
+Vous avez tout dessus, bon courage ! 
+*/
